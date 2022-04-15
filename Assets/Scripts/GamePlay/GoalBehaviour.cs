7b4f1c1d448fccf8716672c501cf5164bda3b8ad
Brittnie <brittniesexton95@gmail.com>
2022-04-14 20:11:14 -0500

    Created ending scene and changed some obstacles

--- Assets/Scripts/GamePlay/GoalBehaviour.cs
+++ Assets/Scripts/GamePlay/GoalBehaviour.cs
@@ -2,24 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class GoalBehaviour : MonoBehaviour
 {
-    [SerializeField]
-    private Text _vitoryText;
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.CompareTag("Finish"))
-    //        _vitoryText.enabled = true;
-    //}
-
     private void OnCollisionEnter(Collision collision)
     {
         
         Debug.Log("Worked");
-        print("Worked");
-        ;
-        
+
+        //Loads the end scene When player has reached the goal
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
--- Assets/Scripts/GamePlay/MainMenuBehaviour.cs
+++ Assets/Scripts/GamePlay/MainMenuBehaviour.cs
@@ -6,9 +6,16 @@ using UnityEngine.SceneManagement;
 public class MainMenuBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void LoadGame()
     {
-
+        //Loads The next scene in the builds queue when the play button is hit
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void ExitApplication()
+    {
+        Debug.Log("Quit!");
+        //Closes the application when the quit game button is hit
+        Application.Quit();
+    }
 }
